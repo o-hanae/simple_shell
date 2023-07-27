66c759983cc4c975be4e635178c571a32b2bf673
Ezzaky-A <aichaezzaky08@gmail.com>
2023-07-27 14:18:21 -0700

    add some change in shell.h

--- shell.h
+++ shell.h
@@ -222,14 +222,5 @@ void free_list(list_t **);
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
-
-/* toem_vars.c */
-int is_chain(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
-int replace_vars(info_t *);
-int replace_string(char **, char *);
 
 #endif
